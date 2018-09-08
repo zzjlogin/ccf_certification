e8cc7772f664456d3bc7c03e3564e52e7538815c
zzjlogin <login_root@163.com>
2018-09-08 13:50:56 +0800

    commit

--- src/201709/201709-4.py
+++ src/201709/201709-4.py
@@ -1,2 +1,96 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
+
+'''
+问题描述
+　　某国的军队由N个部门组成，为了提高安全性，部门之间建立了M条通路，每条通路只能单向传递信息，即一条从部门a到部门b的通路只能由a向b传递信息。信息可以通过中转的方式进行传递，即如果a能将信息传递到b，b又能将信息传递到c，则a能将信息传递到c。一条信息可能通过多次中转最终到达目的地。
+　　由于保密工作做得很好，并不是所有部门之间都互相知道彼此的存在。只有当两个部门之间可以直接或间接传递信息时，他们才彼此知道对方的存在。部门之间不会把自己知道哪些部门告诉其他部门。
+
+　　上图中给了一个4个部门的例子，图中的单向边表示通路。部门1可以将消息发送给所有部门，部门4可以接收所有部门的消息，所以部门1和部门4知道所有其他部门的存在。部门2和部门3之间没有任何方式可以发送消息，所以部门2和部门3互相不知道彼此的存在。
+　　现在请问，有多少个部门知道所有N个部门的存在。或者说，有多少个部门所知道的部门数量（包括自己）正好是N。
+输入格式
+　　输入的第一行包含两个整数N, M，分别表示部门的数量和单向通路的数量。所有部门从1到N标号。
+　　接下来M行，每行两个整数a, b，表示部门a到部门b有一条单向通路。
+输出格式
+　　输出一行，包含一个整数，表示答案。
+样例输入
+4 4
+1 2
+1 3
+2 4
+3 4
+样例输出
+2
+样例说明
+　　部门1和部门4知道所有其他部门的存在。
+评测用例规模与约定
+　　对于30%的评测用例，1 ≤ N ≤ 10，1 ≤ M ≤ 20；
+　　对于60%的评测用例，1 ≤ N ≤ 100，1 ≤ M ≤ 1000；
+　　对于100%的评测用例，1 ≤ N ≤ 1000，1 ≤ M ≤ 10000。
+
+'''
+
+#以下提交是25分：
+
+d_start = {}
+d_end = {}
+c = 0
+a = list(map(int, input().split()))
+n, m = a[0], a[1]
+s = set()
+
+
+def graph(e):
+    if e[0] in d_start:
+        d_start.get(e[0]).append(e[1])
+    else:
+        d_start[e[0]] = [e[1]]
+    if e[1] not in d_start:
+        d_start[e[1]] = []
+
+def graph2(e):
+    if e[0] in d_end:
+        d_end.get(e[0]).append(e[1])
+    else:
+        d_end[e[0]] = [e[1]]
+    if e[1] not in d_end:
+        d_end[e[1]] = []
+
+
+for i in range(m):
+    j = list(map(int, input().split()))
+    for k in j:
+        s.add(k)
+    graph(j)
+    j.reverse()
+    graph2(j)
+
+def dfs_traverse(graph, start):
+    visited, stack = set(), [start]
+    while stack:
+        node = stack.pop()
+        if node not in visited:
+            visited.add(node)
+            if node in graph:
+                for nextNode in graph.get(node):
+                    if nextNode not in visited:
+                        stack.append(nextNode)
+
+    return visited
+
+for i in d_start:
+    p = dfs_traverse(d_start, i)
+
+    if set(p) == s:
+        c += 1
+    else:
+        pass
+
+for i in d_end:
+    p = dfs_traverse(d_end, i)
+    if set(p) == s:
+        c += 1
+    else:
+        pass
+print(c)
+
--- src/201709/201709-5.py
+++ src/201709/201709-5.py
@@ -1,2 +1,51 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
+
+'''
+问题描述
+　　小葱喜欢除法，所以他给了你N个数a1, a2, ⋯, aN，并且希望你执行M次操作，每次操作可能有以下两种：
+　　给你三个数l, r, v，你需要将al, al+1, ⋯, ar之间所有v的倍数除以v。
+　　给你两个数l, r，你需要回答al + al+1 + ⋯ + ar的值是多少。
+输入格式
+　　第一行两个整数N, M，代表数的个数和操作的次数。
+　　接下来一行N个整数，代表N个数一开始的值。
+　　接下来M行，每行代表依次操作。每行开始有一个整数opt。如果opt=1，那么接下来有三个数l, r, v，代表这次操作需要将第l个数到第r个数中v的倍数除以v；如果opt = 2，那么接下来有两个数l, r，代表你需要回答第l个数到第r个数的和。
+输出格式
+　　对于每一次的第二种操作，输出一行代表这次操作所询问的值。
+样例输入
+5 3
+1 2 3 4 5
+2 1 5
+1 1 3 2
+2 1 5
+样例输出
+15
+14
+评测用例规模与约定
+　　对于30%的评测用例，1 ≤ N, M ≤ 1000；
+　　对于另外20%的评测用例，第一种操作中一定有l = r；
+　　对于另外20%的评测用例，第一种操作中一定有l = 1 , r = N；
+　　对于100%的评测用例，1 ≤ N, M ≤ 105，0 ≤ a1, a2, ⋯, aN ≤ 106, 1 ≤ v ≤ 106, 1 ≤ l ≤ r ≤ N。
+
+'''
+
+
+# 以下提交时30分
+
+a = list(map(int, input().split()))
+n, m = a[0], a[1]
+all = list(map(int, input().split()))
+
+
+for i in range(m):
+    stdin = list(map(int, input().split()))
+    if stdin[0]-1:
+        if stdin[1]-1 == stdin[2]:
+            print(all[stdin[1]-1])
+        else:
+            temp = all[stdin[1]-1:stdin[2]]
+            print(sum(temp))
+    else:
+        for j in range(stdin[1]-1, stdin[2]):
+            if all[j]%stdin[3] == 0:
+                all[j] = all[j]//stdin[3]
